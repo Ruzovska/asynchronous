38b6b5bd6763abe770a082aa10c11359660f52f8
Ruzovska <nastybonda@gmail.com>
2018-07-28 15:22:47 +0700

    Fix countdown in makeAlcohool.

--- catpaw.js
+++ catpaw.js
@@ -2,14 +2,18 @@ const app = require('./UnitTasks.js');
 
 function makeAlcohol(a) {
   console.log('Got ' + a.length + ' tasks!')
+  var tasksReady = 0;
+  var tasksLeft = a.length;
   a.map(function(x,i) {
     //x(function(pack) {console.log(pack)});
     //console.log(i+1 + ' task is ready', a.length-i-1 + ' is left!')
     x(function(pack) {
       console.log(pack);
-      console.log(i+1 + ' task is ready', a.length-i-1 + ' is left!');
+      tasksReady = tasksReady + 1;
+      tasksLeft = tasksLeft - 1;
+      console.log(tasksReady + ' task is ready', tasksLeft + ' is left!');
     });
   });
 };
 
-makeAlcohol([app.makeBeer, app.makeWine]);
+makeAlcohol([app.makeWine, app.makeBeer, app.makeWine]);
