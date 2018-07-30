fce8a704a44a874135689392245c103f2a2d47cb
Ruzovska <nastybonda@gmail.com>
2018-07-30 22:04:41 +0700

    Cleanup in catpaw.js.

--- catpaw.js
+++ catpaw.js
@@ -1,29 +1,19 @@
 const app = require('./UnitTasks.js');
-var output;
+
 function makeAlcohol(a,cb) {
-  // console.log('Got ' + a.length + ' tasks!')
   var tasksReady = 0;
   var tasksLeft = a.length;
   packs = [];
-  a.map(function(x,i) {
-    // x(function(pack) {console.log(pack)});
-    // console.log(i+1 + ' task is ready', a.length-i-1 + ' is left!')
+  a.map(function(x) {
     x(function(pack) {
-      // console.log(pack);
-      tasksReady = tasksReady + 1;
-      tasksLeft = tasksLeft - 1;
-      // console.log(tasksReady + ' task is ready', tasksLeft + ' is left!');
-      packPacker(pack,packs);
+      tasksReady += 1;
+      tasksLeft -= 1;
+      packs.push(pack);
       if (tasksLeft === 0) {
         cb(packs);
       };
     });
   });
-  // setTimeout(function() {console.log(packs)}, 10000);
 };
 
 makeAlcohol([app.makeWine, app.makeBeer, app.makeWine], console.log);
-
-function packPacker(inA, outA) {
-  outA.push(inA);
-};
