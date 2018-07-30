592421e92a04831ecbea19c4c7c59b9fdc2992fd
Ruzovska <nastybonda@gmail.com>
2018-07-30 19:27:20 +0700

    Add printing all produced packs.

--- catpaw.js
+++ catpaw.js
@@ -1,9 +1,10 @@
 const app = require('./UnitTasks.js');
-
+var output;
 function makeAlcohol(a,cb) {
   console.log('Got ' + a.length + ' tasks!')
   var tasksReady = 0;
   var tasksLeft = a.length;
+  packs = [];
   a.map(function(x,i) {
     //x(function(pack) {console.log(pack)});
     //console.log(i+1 + ' task is ready', a.length-i-1 + ' is left!')
@@ -12,14 +13,15 @@ function makeAlcohol(a,cb) {
       tasksReady = tasksReady + 1;
       tasksLeft = tasksLeft - 1;
       console.log(tasksReady + ' task is ready', tasksLeft + ' is left!');
-      cb(pack);
+      cb(pack, packs);
     });
   });
+  setTimeout(function() {console.log(packs)}, 10000);
 };
 
 makeAlcohol([app.makeWine, app.makeBeer, app.makeWine], packPacker);
-var oa = [];
-function packPacker(ia) {
-  oa.push(ia);
-  console.log(oa);
+
+function packPacker(inA, outA) {
+  outA.push(inA);
+  console.log(outA);
 };
